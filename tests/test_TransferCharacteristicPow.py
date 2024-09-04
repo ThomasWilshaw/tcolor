e629842adafda99f876781567b7f47e971f1758e
Thomas Wilshaw <thomaswilshaw@gmail.com>
2024-09-04 16:07:49 +0100

    Tests: Use setUp and add inverse test

--- tests/test_TransferCharacteristicPow.py
+++ tests/test_TransferCharacteristicPow.py
@@ -2,23 +2,23 @@ import unittest
 from tcolour import TransferCharacteristic as TC
 
 class TestTransferCharacteristicPow(unittest.TestCase):
+    def setUp(self) -> None:
+        self.TCP = TC.TransferCharacteristicPow(parameters={'a': 2.2})
+
     def test_forward_transfer(self):
-        TCP = TC.TransferCharacteristicPow(parameters={'a': 2.2})
-
         data = [0.0, 0.1, 0.5, 1.0]
-        out = TCP.forward_transfer(data)
+        out = self.TCP.forward_transfer(data)
 
-        check = [pow(x, TCP.parameters["a"]) for x in data]
+        check = [pow(x, self.TCP.parameters["a"]) for x in data]
 
         self.assertListEqual(out, check)
 
     def test_inverse_transfer(self):
-        TCP = TC.TransferCharacteristicPow(parameters={'a': 2.2})
 
         data = [0.0, 0.1, 0.5, 1.0]
-        out = TCP.inverse_transfer(data)
+        out = self.TCP.inverse_transfer(data)
 
-        check = [pow(x, 1.0/TCP.parameters["a"]) for x in data]
+        check = [pow(x, 1.0/self.TCP.parameters["a"]) for x in data]
 
         self.assertListEqual(out, check)
 
--- tests/test_TransferCharacteristicPowWithBreak.py
+++ tests/test_TransferCharacteristicPowWithBreak.py
@@ -2,12 +2,21 @@ import unittest
 from tcolour import TransferCharacteristic as TC
 
 class TestTransferCharacteristicPowWithBreak(unittest.TestCase):
-    def test_forward_transfer(self):
-        TCP = TC.TransferCharacteristicPowWithBreak(parameters={"a": 1.055, "b": -0.055, "c": 12.92, "d": 0.0031308, "g": 2.4})
+    def setUp(self) -> None:
+        self.TCP = TC.TransferCharacteristicPowWithBreak(parameters={"a": 1.055, "b": -0.055, "c": 12.92, "d": 0.0031308, "g": 2.4})
 
+
+    def test_forward_transfer(self):
         data = [0.0, 0.1, 0.5, 1.0]
-        out = TCP.forward_transfer(data)
+        out = self.TCP.forward_transfer(data)
 
         check = [0.0]+[1.055*(pow(x, 1.0/2.4))-0.055 for x in data[1:]]
 
         self.assertListEqual(out, check)
+
+    def test_inverse_transfer(self):
+        data = [0.0, 0.1, 0.5, 1.0]
+        forward = self.TCP.forward_transfer(data)
+        inverse = self.TCP.inverse_transfer(forward)
+
+        self.assertListEqual(data, inverse)
