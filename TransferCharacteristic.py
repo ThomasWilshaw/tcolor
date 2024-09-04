4c5f2ba062648a4b7f016930e21ccdd9274a31cf
Thomas Wilshaw <thomaswilshaw@gmail.com>
2024-09-04 13:30:53 +0100

    TransferCharacteristic: Add forward and inverse transfer function

--- TransferCharacteristic.py
+++ TransferCharacteristic.py
@@ -14,8 +14,12 @@ class TransferCharacteristic():
     def __init__(self, type:TransferCharacteristicType) -> None:
         self.type = type
 
-    def do_transfer(data):
-        """Processes data with the given characteristic transfer function"""
+    def forward_transfer(data):
+        """Processes data with the given characteristic transfer function in the forwards direction"""
+        pass
+
+    def inverse_transfer(data):
+        """Processes data with the given characteristic transfer function in the inverse direction"""
         pass
 
     def __repr__(self) -> str:
@@ -48,10 +52,17 @@ class TransferCharacteristicPow(TransferCharacteristicParametric):
         if len(value) != 1: raise Exception("TransferCharacteristicPow only takes one parameter.")
         self._parameters = value
 
-    def do_transfer(self, data):
+    def forward_transfer(self, data):
         new_data = []
         for idx, x in enumerate(data):
             new_data.append(pow(x, self.parameters.keys[0]))
+
+        return new_data
+
+    def inverse_transfer(self, data):
+        new_data = []
+        for idx, x in enumerate(data):
+            new_data.append(pow(x, 1.0 / self.parameters.keys[0]))
 
         return new_data
 
@@ -74,12 +85,23 @@ class TransferCharacteristicPowWithBreak(TransferCharacteristicParametric):
         if len(value) != 5: raise Exception("TransferCharacteristicPowWithBreak only exactly five parameters.")
         self._parameters = value
 
-    def do_transfer(self, data):
+    def forward_transfer(self, data):
         new_data = []
         for idx, x in enumerate(data):
             item = x
             if(item <= self.parameters["d"]):
                 item = self.parameters["c"] * item
             else:
-                item = self.parameters["a"] * pow(item, self.parameters["g"]) + self.parameters["b"]
+                item = self.parameters["a"] * pow(item, 1.0 / self.parameters["g"]) + self.parameters["b"]
+            new_data.append(item)
+
+    def inverse_transfer(self, data):
+        new_data = []
+        cut_off = self.parameters["c"] * self.parameters["d"]
+        for idx, x in enumerate(data):
+            item = x
+            if(item <= cut_off):
+                item =  item / self.parameters["c"]
+            else:
+                item = pow((item - self.parameters["b"]) / self.parameters["a"], self.parameters["g"])
             new_data.append(item)
