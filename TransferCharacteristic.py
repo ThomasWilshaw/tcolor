5940bc08979adfa7515dd604b5420cb68d707eb5
Thomas Wilshaw <thomaswilshaw@gmail.com>
2024-09-04 12:54:00 +0100

    Initial commit

---
+++ TransferCharacteristic.py
@@ -0,0 +1,85 @@
+from enum import Enum
+
+class TransferCharacteristicType(Enum):
+    URI = 1
+    PARAMETRIC = 2
+    NAMED = 3
+
+class ParametricType(Enum):
+    POWER = 1
+    
+class TransferCharacteristic():
+    """Defines a Transfer Characteristic, stored as either a URI, a Parametric or a Named function."""
+
+    def __init__(self, type:TransferCharacteristicType) -> None:
+        self.type = type
+
+    def do_transfer(data):
+        """Processes data with the given characteristic transfer function"""
+        pass
+
+    def __repr__(self) -> str:
+        return "TransferCharacteristic(type=%r)" % (self.type)
+    
+class TransferCharacteristicParametric(TransferCharacteristic):
+    """A parametric transfer function."""
+
+    def __init__(self, type: TransferCharacteristicType, function:ParametricType, parameters:dict) -> None:
+        super().__init__(type)
+        self.function = function
+        self.parameters = parameters
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+    
+class TransferCharacteristicPow(TransferCharacteristicParametric):
+    """A power function transfer characteristic"""
+
+    def __init__(self, type: TransferCharacteristicType, function=ParametricType.POWER, parameters: dict={"a": 1.0}) -> None:
+        super().__init__(type, function, parameters)
+
+    @property
+    def parameters(self):
+        return self._parameters
+    
+    @parameters.setter
+    def parameters(self, value):
+        if type(value) is not dict: raise Exception("Parameters must be a dict")
+        if len(value) != 1: raise Exception("TransferCharacteristicPow only takes one parameter.")
+        self._parameters = value
+
+    def do_transfer(self, data):
+        new_data = []
+        for idx, x in enumerate(data):
+            new_data.append(pow(x, self.parameters.keys[0]))
+
+        return new_data
+
+    def __repr__(self) -> str:
+        return "TransferCharacteristic(type=%r, function=%r, parameters=%r)" % (self.type, self.function, self.parameters)
+    
+class TransferCharacteristicPowWithBreak(TransferCharacteristicParametric):
+    """A power function with a linear segment near zero transfer characteristic"""
+
+    def __init__(self, type: TransferCharacteristicType, function: ParametricType, parameters: dict) -> None:
+        super().__init__(type, function, parameters)
+
+    @property
+    def parameters(self):
+        return self._parameters
+    
+    @parameters.setter
+    def parameters(self, value):
+        if type(value) is not dict: raise Exception("TransferCharacteristic parameters must be a dict")
+        if len(value) != 5: raise Exception("TransferCharacteristicPowWithBreak only exactly five parameters.")
+        self._parameters = value
+
+    def do_transfer(self, data):
+        new_data = []
+        for idx, x in enumerate(data):
+            item = x
+            if(item <= self.parameters["d"]):
+                item = self.parameters["c"] * item
+            else:
+                item = self.parameters["a"] * pow(item, self.parameters["g"]) + self.parameters["b"]
+            new_data.append(item)
---
+++ colourimetry.py
@@ -0,0 +1,68 @@
+from enum import Enum
+from TransferCharacteristic import *
+
+class RGBPrimaries():
+    """Defines a set of three RGB primaries using the CIE xy coordinate system."""
+
+    def __init__(self, r=[], g=[], b=[]) -> None:
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def __repr__(self) -> str:
+        return "RGBPrimaries(r=%r, g=%r, b=%r)" % (self.r, self.g, self.b)
+    
+class CIEVersion(Enum):
+    CIE_1931_2_DEGREE = 1
+    CIE_2015_2_DEGREE = 2
+    
+
+
+class Colourimetry:
+    """Holds all the data required to define a colour space\n
+    Attributes:\n
+        descriptor:                 A unique identifying key.
+        primaries:                  An array defining a set of three RGB primaries using the CIE xy coordinate system.
+        achromatic:                 A single array defining a pair of CIE xy coordinates.
+        transfer_characteristic:    Either a file, parametric function or named function.
+        hints:                      Dictionary providing ancillary colourimetric information.
+        alias:                      An array of strings representing aliases for the chosen descriptor.
+        cie_version:                An enum of predefined CIE versions 
+
+    """
+    def __init__(self, descriptor:str="", rgb_primaries:RGBPrimaries=RGBPrimaries(), achromatic:list=[], 
+                 transfer_characteristic:TransferCharacteristic=None, hints:dict={}, alias:dict={},
+                 cie_version:CIEVersion=None) -> None:
+        self.descriptor = descriptor
+        self.primaries = rgb_primaries
+        self.achromatic = achromatic
+        self.transfer_characteristic = transfer_characteristic
+        self.hints = hints
+        self.alias = alias
+        self.cie_version = cie_version
+
+    def __repr__(self) -> str:
+        return "Colourimetry(descriptor=%r,primaries=%r, achromatic=%r, transfer_characteristic=%r, hints=%r, alias=%r, cie_version=%r)" \
+            % (self.descriptor, self.primaries, self.achromatic, self.transfer_characteristic, self.hints, self.alias, self.cie_version)
+
+
+if __name__ == "__main__":
+    col = Colourimetry()
+    print(col.__doc__)
+
+    col.descriptor = "sRGB"
+
+    col.primaries.r = [0.64, 0.33]
+    col.primaries.g = [0.3, 0.6]
+    col.primaries.b = [0.15, 0.06]
+
+    col.achromatic = [0.3127, 0.329]
+
+    col.transfer_characteristic = TransferCharacteristicPow(TransferCharacteristicType.PARAMETRIC, parameters={'a': 2.2})
+    col.hints["key"] = "value"
+    col.alias["key"] = "value"
+
+    col.cie_version = CIEVersion.CIE_1931_2_DEGREE
+
+    print(repr(col))
+
