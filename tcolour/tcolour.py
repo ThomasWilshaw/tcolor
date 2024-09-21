295ac15465f785131f43dce344677c1b69d423ad
Thomas Wilshaw <thomaswilshaw@gmail.com>
2024-09-21 17:12:34 +0100

    tcolour: Add support for paramtric functions
    Initially only supporting power and powerwithbreak.
    
    Also add two new test files

--- tcolour/tcolour.py
+++ tcolour/tcolour.py
@@ -1,5 +1,6 @@
 import yaml
 import Colourimetry
+import TransferCharacteristic
 
 class TColor():
     """Contains a set of known colour spaces. Allows for interacting with and 
@@ -26,43 +27,75 @@ class TColor():
         except KeyError as e:
             print("YAML ERROR: ", e)
 
+    def transfer_charactersitc_from_YAML(self, yaml_input):
+        try:
+            tc_type = yaml_input[0]["Type"]
+            
+            if tc_type == "Parametric":
+                function = yaml_input[1]["Function"]
+                if function == "powerwithbreak":
+                    return TransferCharacteristic.TransferCharacteristicPowerWithBreak(yaml_input[2]["Parameters"])
+                elif function == "power":
+                    return TransferCharacteristic.TransferCharacteristicPower(yaml_input[2]["Parameters"])
+                else:
+                    raise ValueError("Transfer Characteristic function not supported", function)
+
+            elif tc_type == "Sequence":
+                raise ValueError("Transfer Characteristic type Sequece not yet supported", yaml_input)
+
+            elif tc_type == "URI":
+                raise ValueError("Transfer Characteristic type URI not yet supported", yaml_input)
+
+            else:
+               raise ValueError("YAML ERROR, invald Transfer Characteristic type: ", yaml_input)
+            
+        except KeyError as e:
+            print("YAML ERROR: ", e)
+
 
     def parse_data(self, data:list):
         """Parse the YAML data"""
         for id, idx in enumerate(data):
-            new_colourimetry_set = Colourimetry.Colourimetry()
-            name = list(data[id].keys())[0]
-            colour_space = data[id][name]
-            
-            new_colourimetry_set.descriptor = name
+            try:
+                new_colourimetry_set = Colourimetry.Colourimetry()
+                name = list(data[id].keys())[0]
+                colour_space = data[id][name]
 
-            if "RGB Primaries" in colour_space:
-                primaries = colour_space["RGB Primaries"]
-                if type(primaries) is dict:
-                    new_colourimetry_set.primaries = self.RGBPrimaries_from_YAML(primaries)
-                else:
-                    new_colourimetry_set.primaries = primaries
+                new_colourimetry_set.descriptor = name
 
-            if "Achromatic Centroid" in colour_space:
-                achromatic_centroid = colour_space["Achromatic Centroid"]
-                if type(achromatic_centroid) is dict:
-                    new_colourimetry_set.achromatic = self.achromatic_centroid_from_YAML(achromatic_centroid)
-                else:
-                    new_colourimetry_set.achromatic = achromatic_centroid
+                if "RGB Primaries" in colour_space:
+                    primaries = colour_space["RGB Primaries"]
+                    if type(primaries) is dict:
+                        new_colourimetry_set.primaries = self.RGBPrimaries_from_YAML(primaries)
+                    else:
+                        new_colourimetry_set.primaries = primaries
 
-            if "Transfer Characteristic" in colour_space:
-                new_colourimetry_set.transfer_characteristic = colour_space["Transfer Characteristic"]
+                if "Achromatic Centroid" in colour_space:
+                    achromatic_centroid = colour_space["Achromatic Centroid"]
+                    if type(achromatic_centroid) is dict:
+                        new_colourimetry_set.achromatic = self.achromatic_centroid_from_YAML(achromatic_centroid)
+                    else:
+                        new_colourimetry_set.achromatic = achromatic_centroid
 
-            if "Hints" in colour_space:
-                new_colourimetry_set.hints = colour_space["Hints"]
+                if "Transfer Characteristic" in colour_space:
+                    transfer_characteristic = colour_space["Transfer Characteristic"]
+                    if type(transfer_characteristic) is list:
+                        new_colourimetry_set.transfer_characteristic = self.transfer_charactersitc_from_YAML(transfer_characteristic)
+                    else:
+                        new_colourimetry_set.transfer_characteristic = transfer_characteristic
 
-            if "Alias" in colour_space:
-                new_colourimetry_set.alias = colour_space["Alias"]
+                if "Hints" in colour_space:
+                    new_colourimetry_set.hints = colour_space["Hints"]
 
-            if "CIE Version" in colour_space:
-                new_colourimetry_set.cie_version = colour_space["CIE Version"]
+                if "Alias" in colour_space:
+                    new_colourimetry_set.alias = colour_space["Alias"]
 
-            self.config[str(name)] = new_colourimetry_set
+                if "CIE Version" in colour_space:
+                    new_colourimetry_set.cie_version = colour_space["CIE Version"]
+
+                self.config[str(name)] = new_colourimetry_set
+            except Exception as e:
+                print(e, "Skipping this Colourimetry chunk")
                 
 
 
@@ -83,3 +116,4 @@ if __name__ == "__main__":
 
     for key, value in config.config.items():
         print(key, value)
+        pass
